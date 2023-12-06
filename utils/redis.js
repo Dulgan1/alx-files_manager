b2f58ed600d3a680c08b73ff322bf64d492ba3da
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-06 13:33:18 +0100

    Initial commiy

---
+++ utils/redis.js
@@ -0,0 +1,29 @@
+const redis = require('redis');
+
+class RedisClient() {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.log(error.message));
+  }
+
+  isAlive() {
+    this.client.connected;
+  }
+
+  async get(key) {
+    return await this.client.get(key);
+  }
+
+  async set(key, val, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+redisClient = new RedisClient();
+
+module.exports = redisClient;
