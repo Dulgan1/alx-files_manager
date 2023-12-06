1ed87e2ff8777de677bf554ff43f29a394f31fbf
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-06 07:18:13 -0800

    ESlint

--- utils/redis.js
+++ utils/redis.js
@@ -8,16 +8,17 @@ class RedisClient {
   }
 
   isAlive() {
-    this.client.connected;
+    return this.client.connected;
   }
 
   async get(key) {
     const getVal = await util.promisify(this.client.get).bind(this.client);
-    return await getVal(key);
+    const v = await getVal(key);
+    return v;
   }
 
   async set(key, val, duration) {
-    await this.client.set(key, value);
+    await this.client.set(key, val);
     await this.client.expire(key, duration);
   }
 
