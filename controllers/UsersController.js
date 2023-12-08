85f6c356c83c34672a296c5987cfdb397376db3e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-07 16:45:54 -0800

    user controller and new route

---
+++ controllers/UsersController.js
@@ -0,0 +1,48 @@
+import { ObjectId } from 'mongodb';
+
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+const crypto = require('crypto');
+
+class UserController {
+  static async postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
+    const existsData = await dbClient.db.collection('users').find({ email }).toArray();
+
+    if (!email) {
+      return (response.status(400).json({ error: 'Missing email' }));
+    } else if (!password) {
+      return (response.status(400).json({ error: 'Missing password' }));
+    } else if (existsData.length) {
+      return (response.status(400).json({ error: 'Already exist' }));
+    }
+
+    const hashedPW = cryptPassword(password);
+    const user = await dbClient.db.collection('users').insertOne({ email, password: hashedPW});
+    const resData = { id: user.ops[0]._id, email: user.ops[0].email };
+
+    return (response.status(201).json(resData));
+  }
+
+  static async getMe(request, response) {
+    const key = request.header('X-Token');
+    const session = await redisClient.get(`auth_${key}`);
+    if (!key || key.length === 0) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+    if (session) {
+      const search = await dbClient.db.collection('users').find({ _id: ObjectId(session) }).toArray();
+      return (response.status(200).json({ id: search[0]._id, email: search[0].email }));
+    }
+    return (response.status(401).json({ error: 'Unauthorized' }));
+  }
+}
+
+function cryptPassword(password) {
+  const hash = crypto.createHash('sha1');
+  const hashedPW = hash.update(password, 'utf-8');
+  return hashedPW.disgest('hex');
+}
+
+module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UserController = require('../controller/UserController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UserController.postNew);
 
 module.exports = router;
